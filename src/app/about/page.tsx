57b0f74be1ef1168cf734b0ba2cbb2d5c1f688bf
Wellington Vinhatto <145069076+W-vinhatto@users.noreply.github.com>
2024-08-03 16:37:42 -0300

    chore: add githuck

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,12 @@
-import { Avatar, AvatarFallback, AvatarImage } from '../_components/ui/avatar'
-
+import { Avatar, AvatarFallback, AvatarImage } from "../_components/ui/avatar"
 
 const About = () => {
-    return (
-            <Avatar>
-                <AvatarImage src="https://github.com/shadcn.png" />
-                <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-        )
+  return (
+    <Avatar>
+      <AvatarImage src="https://github.com/shadcn.png" />
+      <AvatarFallback> CN</AvatarFallback>
+    </Avatar>
+  )
 }
 
 export default About
